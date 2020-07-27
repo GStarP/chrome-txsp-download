bfef40b48b236ed548b9bbdcc0d7a45a08e69fba
GStarP <171250605@smail.nju.edu.cn>
2020-07-27 21:07:38 +0800

    feat: 下载 m3u8 文件

--- txsp.js
+++ txsp.js
@@ -83,7 +83,18 @@ var xhrProxy = function (options) {
 /* 代理全局 XHR 完毕 */
 console.log('=== proxy xhr ===')
 
-var videoInfo = {}
+var videoReqInfo = {
+  videoUrl: '',
+  videoBody: null
+}
+
+var videoInfo = {
+  name: '',
+  size: 0,
+  width: 0,
+  height: 0,
+  m3u8url: ''
+}
 
 xhrProxy({
   open: function ([method, url], xhr) {
@@ -96,35 +107,96 @@ xhrProxy({
   },
   onreadystatechange: function (xhr) {
     if (xhr.readyState === 4 && xhr.body) {
-      Object.assign(videoInfo, {
-        videoUrl: xhr.url,
-        videoBody: xhr.body
-      })
-      parseVideoReq(xhr.responseText)
+      videoReqInfo.videoUrl = xhr.url
+      videoReqInfo.videoBody = xhr.body
+      parseVideoReqInfo(xhr.responseText)
     }
   }
 })
 
-function parseVideoReq(res) {
+function parseVideoReqInfo(res) {
+  // 解析获取到的视频总体信息
   var data = typeof res == "string" ? JSON.parse(res) : res
-  var vinfo = JSON.parse(data.vinfo);
-  console.log(vinfo)
-}
+  var vinfo = JSON.parse(data.vinfo)
 
+  console.log('vinfo\n', vinfo)
 
-window.onload = function () {
-  console.log('=== TXSP Downloader Start ===')
-
-  var txsp = document.createElement('div')
-  txsp.classList.add('txsp')
-  txsp.innerHTML = 'download'
-  txsp.addEventListener('click', () => {
-    downloadTXSPVideo()
+  // 只需要进行最高画质的下载
+  var { name: defn, cname: defDesc } = vinfo.fl.fi[vinfo.fl.fi.length - 1]
+  var task = new Promise(resolve => {
+    var body = videoReqInfo.videoBody.replace(/defn=[^&]*/, 'defn=' + defn)
+    // 获取最高画质视频的基本信息
+    fetch(videoReqInfo.videoUrl, {
+        body: body,
+        method: 'POST',
+      })
+      .then(res => res.json())
+      .then(data => ({ data }))
+      .then(resolve)
   })
 
+  task.then(last => {
+    // 解析得到视频基本信息
+    var { url, width, height, size } = parseVideoInfo(last.data)
+    videoInfo.name = document.title.split('_')[0]
+    videoInfo.size = size
+    videoInfo.width = width
+    videoInfo.height = height
+    videoInfo.m3u8url = url
+
+    console.log(`
+      ${videoInfo.name}\n
+      ${videoInfo.width}x${videoInfo.height}\n
+      ${videoInfo.size}M\n
+      ${videoInfo.m3u8url}
+    `)
+
+    showDownloadBtn()
+
+  })
+  
+}
+
+// 解析获取到的视频基本信息
+function parseVideoInfo(data) {
+  var vinfo = JSON.parse(data.vinfo)
+  var vi = vinfo.vl.vi[0]
+  var ui = vi.ul.ui[0]
+  var url = ui.url
+  if (url.indexOf('.m3u8') == -1) {
+    url += ui.hls.pt
+  }
+  var { vw: width, vh: height, fs: size } = vi
+  size = Math.floor(size / 1024 / 1024)
+  return { url, width, height, size }
+}
+
+// 显示下载按钮
+function showDownloadBtn () {
+  var txsp = document.createElement('div')
+  txsp.classList.add('txsp')
+  txsp.innerHTML = `${videoInfo.width} x ${videoInfo.height}  ${videoInfo.size} M`
+  txsp.addEventListener('click', () => {
+    downloadVideo()
+  })
   document.body.appendChild(txsp)
 }
 
-function downloadTXSPVideo() {
+// 根据 m3u8 文件下载视频
+function downloadVideo() {
+  fetch(videoInfo.m3u8url)
+    .then(res => res.text())
+    .then(data => {
+      downloadFile(`${videoInfo.name}.m3u8`, data)
+    })
+}
 
+function downloadFile(filename, json) {
+  let el = document.createElement('a')
+  el.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(json))
+  el.setAttribute('download', filename)
+  el.style.display = 'none'
+  document.body.appendChild(el)
+  el.click()
+  document.body.removeChild(el)
 }
